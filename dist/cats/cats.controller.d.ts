3caf76a5ef5b03049c77bcd592743648a70f3f97
somsak <somsakk@tisco.co.th>
2025-04-02 22:12:37 +0700

    init project v2.

--- dist/cats/cats.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { CatsService } from './cats.service';
-export declare class CatsController {
-    private readonly catsService;
-    constructor(catsService: CatsService);
-    getCats(): string[];
-    getCatByName(name: string): string;
-}
