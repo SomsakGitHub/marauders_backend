7691690f514d32bb1cf4be8edf815d17243ed90d
somsak <somsakk@tisco.co.th>
2025-04-03 21:59:52 +0700

    Add DB.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma.service';
 
 @Injectable()
 export class AppService {
+
+  constructor(private prisma: PrismaService) {}   
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createTest(data: any) {
+    return this.prisma.tset_db.create({
+      data,
+    });
   }
 }
